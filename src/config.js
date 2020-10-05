6d162177e7167e18d24138652a8b83f11c11e3fb
agarwri <ritika.agarwal1995@gmail.com>
2020-10-05 16:14:29 -0400

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "ritika-scratch-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://hhi1x29ps7.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_dgfzxDCus",
+    APP_CLIENT_ID: "6simd958n8376dmt7cmdct01is",
+    IDENTITY_POOL_ID: "us-east-2:374a0a05-9f22-4326-8fac-64d3cedbd8f4"
+  }
+};
