f1c4ff332359ddbf903c9b9663c4085777df1025
agarwri <ritika.agarwal1995@gmail.com>
2020-10-25 12:55:06 -0400

    Building our react app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51HXzPfEng2kidofDvWYJvk8K392CsBOTmlCXIsogYMxU2AA7ILJsfp04FBarYvlh4HcV9GRkA2aROjAE9ajxnPTi008h6f3M8t",
   s3: {
     REGION: "us-east-1",
     BUCKET: "ritika-scratch-app-uploads"
@@ -13,4 +15,5 @@ export default {
     APP_CLIENT_ID: "6simd958n8376dmt7cmdct01is",
     IDENTITY_POOL_ID: "us-east-2:374a0a05-9f22-4326-8fac-64d3cedbd8f4"
   }
+
 };
